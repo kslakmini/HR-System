c1a224a5786454c9f8702784d66ad2e1812649f7
sakuni2000 <sakunidananjana@gmail.com>
2024-09-05 22:21:21 +0530

    Add commit

---
+++ src/main/java/com/HR/System/HRsystem/HRsystemApplication.java
@@ -0,0 +1,13 @@
+package com.HR.System.HRsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HRsystemApplication.class, args);
+	}
+
+}
